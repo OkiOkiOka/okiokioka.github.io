282e78790935adead0caa42e6fe03a72288604dd
OkiOkiOka <cherry960518@gmail.com>
2022-05-12 19:50:20 +0800

    “okiokioka.github.io“

---
+++ inspiMemories.js
@@ -0,0 +1,22 @@
+window.addEventListener('load', function(){
+    $('html').toggleClass('loading')
+  });
+  
+   let secretCode = [79,75,73];
+  //宣告一個含有指令串keyCode的陣列
+  let userInput = [];   //宣告一個可接收用戶輸入的keyCode的空陣列
+  window.addEventListener('keyup',function(e){
+    console.log('typing...')
+      userInput.push(e.keyCode);   //在末端新增用戶新輸入的keyCode
+      userInput.splice(-secretCode.length - 1, userInput.length - secretCode.length);
+      //userInput.splice( 起始值 / 新增keyCode的放置位置,  );
+      if(userInput.join() === secretCode.join()){
+          location.href = 'https://ithelp.ithome.com.tw/questions'
+      };
+  });
+  //userInput.splice( 起始值 / 新增keyCode的放置位置,  );
+
+
+  import swal from 'sweetalert';
+  swal("Hello world!");
+  console.log('Test');
